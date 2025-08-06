9b4c7be8adfb4a6b84ed9b1d4a0434e71f30fba4
tung nguyen <tngu0492@student.monash.edu>
2025-08-07 00:52:45 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Json from './components/JSON.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Json />
+  </main>
+</template>
+
+<style scoped>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #ffffff;
+  margin: 0;
+}
+main {
+  padding: 20px;
+  background-color: #ffffff;
+}
+header {
+  background-color: #35495e;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  font-size: 24px;
+}
+</style>
